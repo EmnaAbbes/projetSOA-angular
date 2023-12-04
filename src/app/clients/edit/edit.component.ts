364858f917286c7888149834caf7ff89aaf9d1a6
EmnaAbbes <emna.abbes5@gmail.com>
2023-12-04 13:17:42 +0100

    new commit

--- src/app/clients/edit/edit.component.ts
+++ src/app/clients/edit/edit.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input  } from '@angular/core';
+import { ClientService } from '../client.service';
+import { Client } from '../client';
+
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
 
+  @Input() clientId: number;
+
+  @ViewChild('myModal') myModal!: ElementRef;
+
+  display = "none";
+
+  clients:Client=new Client();
+
+  constructor(private clientserv:ClientService){}
+  ngOnInit(){
+   this.clientserv.find(this.clientId).subscribe(data => {
+    this.clients = data;
+  });
+  }
+
+  editClient=()=>{
+     this.clientserv.edit(this.clients.id,this.clients).subscribe((data=>{
+      console.log(data)
+      this.closeModal() 
+      window.location.reload();
+  }))
+    
+  }
+  
+  openModal() { 
+       this.display = "block";
+  }
+ 
+  closeModal() {
+    this.display = "none";
+  }
+  
 }
