cc3f91242478d61e91cccad0d9bb4a17d5f74531
khouloudbdiri <khouloudbdiri11@gmail.com>
2023-12-10 13:14:57 +0100

    dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  totalClients: number;
+  newClients: number;
 
+  constructor(private dashboardService: DashboardService) { }
+
+  ngOnInit(): void {
+    this.fetchClientStatistics();
+  }
+
+  fetchClientStatistics(): void {
+    this.dashboardService.getClientStatistics().subscribe(
+      data => {
+        this.totalClients = data.totalClients;
+        this.newClients = data.newClients;
+        console.log('Total Clients:', this.totalClients);
+        console.log('New Clients:', this.newClients);
+      },
+      error => {
+        console.error('Error fetching client statistics:', error);
+      }
+    );
+  }
 }
--- src/app/dashboard/top-widgets/top-widgets.component.ts
+++ src/app/dashboard/top-widgets/top-widgets.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../dashboard.service';
 
 @Component({
   selector: 'app-top-widgets',
   templateUrl: './top-widgets.component.html',
   styleUrls: ['./top-widgets.component.css']
 })
-export class TopWidgetsComponent {
+export class TopWidgetsComponent implements OnInit {
+  totalClients: number;
+  newClients: number;
 
+  constructor(private dashboardService: DashboardService) { }
+
+  ngOnInit(): void {
+    this.dashboardService.getClientStatistics().subscribe(data => {
+      this.totalClients = data.totalClients;
+      this.newClients = data.newClients;
+    });
+  }
 }
