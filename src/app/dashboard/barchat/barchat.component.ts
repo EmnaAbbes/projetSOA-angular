6004b0aba50ff414ad9410e199af64e4e153e016
khouloudbdiri <khouloudbdiri11@gmail.com>
2023-12-13 14:16:22 +0100

    final

--- src/app/dashboard/barchat/barchat.component.ts
+++ src/app/dashboard/barchat/barchat.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
-
+import { DashboardService } from '../dashboard.service';
+import { PointOptionsObject } from 'highcharts';
+import { count } from 'rxjs';
+interface YearCounts {
+  year: number;
+  count: number;
+}
 @Component({
   selector: 'app-barchat',
   templateUrl: './barchat.component.html',
@@ -16,21 +22,19 @@ export class BarchatComponent implements OnInit {
       text: 'Nombre des clients au cours des années',
       align: 'center'
     },
-    
     xAxis: {
-      categories: ['2018', '2019', '2020', '2021', '2022', '2023'],
+      categories: [],
       crosshair: true,
       accessibility: {
-        description: 'Countries'
+        description: 'Years'
       }
     },
     yAxis: {
       min: 0,
       title: {
-        text: 'nombre des clients'
-      }
+        text: 'Nombre des clients'
+      },
     },
-   
     plotOptions: {
       column: {
         pointPadding: 0.2,
@@ -41,19 +45,48 @@ export class BarchatComponent implements OnInit {
       {
         name: 'Clients',
         type: 'column',
-        color:'#98EBED',
-        data: [120,145,210,187,260,320]
+        color: '#98EBED',
+        data: []
       },
-      
     ],
     credits: {
       enabled: false
     }
   });
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
+    this.dashboardService.getYearClientCounts().subscribe(
+      (yearCounts:YearCounts) => {
+        if (yearCounts && typeof yearCounts === 'object') {
+          const data: PointOptionsObject[] = Object.keys(yearCounts).map(year => ({
+            name: year,
+            y:yearCounts.count
+          }));
 
+          const years = Object.keys(yearCounts);
+          const count = Object.values(yearCounts);
+
+          if (this.chart.ref?.xAxis[0]) {
+            this.chart.ref.xAxis[0].setCategories(years, false);
+            console.log(yearCounts)
+          } else {
+            console.log('X-axis not found.');
+          }
+          if (this.chart.ref) {
+            this.chart.ref.series[0].setData(count, true);
+          } else {
+            console.log('Chart series not found.');
+          }
+          
+        } else {
+          console.error('Received data is not an object:', yearCounts);
+        }
+      },
+      error => {
+        console.error('Error fetching year counts:', error);
+      }
+    );
   }
 }
